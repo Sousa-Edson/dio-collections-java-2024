2cec2c11b326e14e849fc471d0350e8d2e26bf63
Edson Sousa <edson3711.es@gmail.com>
2024-05-03 08:21:37 -0300

    adiciona generics

---
+++ src/generics/GenericsExempleList.java
@@ -0,0 +1,26 @@
+package generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenericsExempleList {
+
+  public static void main(String[] args) {
+    List listaSemGenerics = new ArrayList();
+    listaSemGenerics.add("Elemento 1");
+    listaSemGenerics.add(10);
+ 
+    List<String> listaGenerics = new ArrayList<>();
+    listaGenerics.add("Elemento 1");
+    listaGenerics.add("Elemento 2");
+
+    for (String elemento : listaGenerics) {
+      System.out.println(elemento);
+    }
+
+    for (Object elemento : listaSemGenerics) {
+      String str = (String) elemento;
+      System.out.println(str);
+    }
+  }
+}
